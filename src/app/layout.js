f42b1f8c3b70d1c24e2ec885bb61b6d3fc0cb580
Dawid Klos <klosdawid12@gmail.com>
2024-01-19 17:23:25 +0000

    feat: add 2 Proxima Nova font variants

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,12 +5,20 @@ const proximaNova = localFont({
   name: "Proxima Nova",
   src: [
     {
+      path: "./fonts/proximanova_light.woff2",
+      weight: "200",
+    },
+    {
       path: "./fonts/proximanova_regular.woff2",
       weight: "400",
     },
     {
       path: "./fonts/proximanova_semibold.woff2",
       weight: "600",
+    },
+    {
+      path: "./fonts/proximanova_bold.woff2",
+      weight: "700",
     },
   ]
 });
