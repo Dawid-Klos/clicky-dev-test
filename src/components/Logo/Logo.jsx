1b592e487ca61f8cf858eff51e6bde7436fa830a
Dawid Klos <klosdawid12@gmail.com>
2024-01-20 20:07:41 +0000

    refactor: Logo component missing BEMIT classname

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -7,8 +7,8 @@ import './Logo.scss';
 
 const Logo = () => {
     return (
-        <Link className='logo' href="/">
-            <Image className='logo__image' src={logo} alt="ClickyGroup Logo" width={128} height={77} priority />
+        <Link className='c-logo' href="/">
+            <Image className='c-logo__image' src={logo} alt="ClickyGroup Logo" width={102} height={62} priority />
         </Link>
     );
 };
