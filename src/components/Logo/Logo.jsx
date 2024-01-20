b81a3a3d362f7737f56df1fed7c7e75522551275
Dawid Klos <klosdawid12@gmail.com>
2024-01-20 18:48:19 +0000

    feat: create Logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import logo from 'assets/group.svg';
+
+import './Logo.scss';
+
+const Logo = () => {
+    return (
+        <Link className='logo' href="/">
+            <Image className='logo__image' src={logo} alt="ClickyGroup Logo" width={128} height={77} />
+        </Link>
+    );
+};
+
+export default Logo;
