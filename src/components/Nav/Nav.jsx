4e56199c432de823626784b7a62437ae611bb751
Dawid Klos <klosdawid12@gmail.com>
2024-01-21 14:44:33 +0000

    refactor: Nav component classes

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,21 +3,23 @@ import Link from 'next/link';
 import { navItems } from 'data/navItems';
 import './Nav.scss';
 
-const Nav = () => {
-
+const Nav = ({ isMenuOpen, toggleMenu }) => {
 
     return (
-        <ul className='nav'>
-            {navItems.map((item, index) => {
-                return (
-                    <li className='nav__item' key={item.label + "_" + index}>
-                        <Link className='nav__link' href={item.href}>
-                            {item.label}
-                        </Link>
-                    </li>
-                );
-            })}
-        </ul>
+        <nav className={`c-nav ${isMenuOpen && 'c-nav--is-open'}`}>
+            <ul className='c-nav__list'>
+                {navItems.map((item, index) => {
+                    return (
+                        <li className='c-nav__item' key={item.label + "_" + index}>
+                            <Link className='c-nav__link' href={item.href} onClick={toggleMenu}>
+                                {item.label}
+                            </Link>
+                        </li>
+                    );
+                })}
+            </ul>
+            <p className='c-nav__copy'>&copy; ClickyGroup 2024</p>
+        </nav>
     );
 };
 
