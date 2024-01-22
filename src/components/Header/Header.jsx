8d50b53dcdd073f56bf10265a19a982c494a2915
Dawid Klos <klosdawid12@gmail.com>
2024-01-22 11:58:33 +0000

    fix: nav taking page space even when closed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -31,20 +31,21 @@ const Header = () => {
     } else {
         return (
             <header className={`c-header ${isMenuOpen && 'c-header--is-active'}`}>
-                <Logo />
+                <div className="c-header__wrapper">
+                    <Logo />
+                    <button className='c-header__menu-toggle'
+                        onClick={toggleMenu}
+                        aria-haspopup="true"
+                        aria-expanded={isMenuOpen ? 'true' : 'false'}
+                        aria-label="Toggle menu">
+                        <svg className={`c-icon ${isMenuOpen && 'c-icon--is-open'}`} viewBox="0 0 100 64" width="32" height="32" aria-hidden="true">
+                            <rect className='c-icon__rect-top' width="100" height="10" rx="6"></rect>
+                            <rect className='c-icon__rect-middle' y="30" width="100" height="10" rx="6"></rect>
+                            <rect className='c-icon__rect-bottom' y="60" width="100" height="10" rx="6"></rect>
+                        </svg>
 
-                <button className='c-header__menu-toggle'
-                    onClick={toggleMenu}
-                    aria-haspopup="true"
-                    aria-expanded={isMenuOpen ? 'true' : 'false'}
-                    aria-label="Toggle menu">
-                    <svg className={`c-icon ${isMenuOpen && 'c-icon--is-open'}`} viewBox="0 0 100 64" width="32" height="32" aria-hidden="true">
-                        <rect className='c-icon__rect-top' width="100" height="10" rx="6"></rect>
-                        <rect className='c-icon__rect-middle' y="30" width="100" height="10" rx="6"></rect>
-                        <rect className='c-icon__rect-bottom' y="60" width="100" height="10" rx="6"></rect>
-                    </svg>
-
-                </button>
+                    </button>
+                </div>
 
                 <Divider color='blue' />
                 <Nav isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
