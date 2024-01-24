2161d25931fa018f24a7099f3dfc2f4bffcd40c8
Dawid Klos <klosdawid12@gmail.com>
2024-01-24 18:21:37 +0000

    feat: add carousel data for homepage testimonials

---
+++ src/data/carousel.js
@@ -0,0 +1,38 @@
+import bgTestimonialOne from 'assets/slider-images/testimonials/slide-1.webp';
+import bgTestimonialTwo from 'assets/slider-images/testimonials/slide-2.webp';
+import bgTestimonialThree from 'assets/slider-images/testimonials/slide-3.webp';
+
+import avatartTestimonialOne from 'assets/slider-images/testimonials/avatar-slide-1.webp';
+import avatartTestimonialTwo from 'assets/slider-images/testimonials/avatar-slide-2.webp';
+import avatartTestimonialThree from 'assets/slider-images/testimonials/avatar-slide-3.webp';
+
+
+export const homepageTestimonials = [
+    {
+        id: "homepage_1",
+        name: 'Name',
+        role: 'Role',
+        company: 'Company',
+        avatar: avatartTestimonialTwo,
+        description: '“Praesent id congue magna. Duis vitae fermentum sem, id dignissim augue. Nullam ullamcorper nibh magna, et lacinia nulla efficitur in. Donec eros est, varius ut nibh in. Hendrerit pharetra orci. Vivamus orci urna, dictum sed orci mattis, eleifend varius ante.”',
+        background: bgTestimonialTwo
+    },
+    {
+        id: "homepage_2",
+        name: 'Name',
+        role: 'Role',
+        company: 'Company',
+        avatar: avatartTestimonialOne,
+        description: '“Praesent id congue magna. Duis vitae fermentum sem, id dignissim augue. Nullam ullamcorper nibh magna, et lacinia nulla efficitur in. Donec eros est, varius ut nibh in. Hendrerit pharetra orci. Vivamus orci urna, dictum sed orci mattis, eleifend varius ante.”',
+        background: bgTestimonialOne
+    },
+    {
+        id: "homepage_3",
+        name: 'Name',
+        role: 'Role',
+        company: 'Company',
+        avatar: avatartTestimonialThree,
+        description: '“Praesent id congue magna. Duis vitae fermentum sem, id dignissim augue. Nullam ullamcorper nibh magna, et lacinia nulla efficitur in. Donec eros est, varius ut nibh in. Hendrerit pharetra orci. Vivamus orci urna, dictum sed orci mattis, eleifend varius ante.”',
+        background: bgTestimonialThree
+    }
+];
