18ad30bbae60c59c9de5069f46e04510fa5dc4a1
Dawid Klos <klosdawid12@gmail.com>
2024-01-24 18:20:42 +0000

    feat: add SliderNav component with styles

---
+++ src/components/Testimonials/SliderNav/SliderNav.jsx
@@ -0,0 +1,27 @@
+import { useSwiper } from "swiper/react";
+
+import "./SliderNav.scss";
+
+
+const SliderNav = ({ testimonials, activeSlide }) => {
+    const swiper = useSwiper();
+
+    const handleSlide = (index) => {
+        swiper.slideTo(index);
+    }
+
+    return (
+        <div className="c-slider-nav">
+            {testimonials.map((testimonial, index) => (
+                <button
+                    className={`c-slider-nav__button ${index === activeSlide ? "c-slider-nav__button--active" : ""}`}
+                    key={index}
+                    onClick={() => handleSlide(index)}
+                    type="button"
+                ></button>
+            ))}
+        </div>
+    );
+};
+
+export default SliderNav;
