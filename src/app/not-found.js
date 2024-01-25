46945a4db9f4efa3b3857ed9520d5eca911f1c58
Dawid Klos <klosdawid12@gmail.com>
2024-01-25 15:55:52 +0000

    feat: create not-found component with styles

---
+++ src/app/not-found.js
@@ -0,0 +1,20 @@
+import Header from 'components/Header/Header';
+import Footer from 'components/Footer/Footer';
+import Button from 'components/common/Button/Button';
+
+import './not-found.scss';
+
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <section className="c-not-found">
+                <h1 className="c-not-found__title">Oops - Page not found</h1>
+                <p className="c-not-found__description">Sorry, but the page you are looking for does not exist.</p>
+                <Button title="GO BACK" color="blue" />
+            </section>
+            <Footer />
+        </>
+    );
+}
