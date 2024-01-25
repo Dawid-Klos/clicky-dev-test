f52a68eb2d843b3938663d4fedf48d13ece433be
Dawid Klos <klosdawid12@gmail.com>
2024-01-25 17:00:23 +0000

    style: update not-found page to include image

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,6 +1,10 @@
+import Image from 'next/image';
+
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
 import Button from 'components/common/Button/Button';
+
+import NotFoundImg from 'assets/img-404.svg';
 
 import './not-found.scss';
 
@@ -10,7 +14,8 @@ export default function Home() {
         <>
             <Header />
             <section className="c-not-found">
-                <h1 className="c-not-found__title">Oops - Page not found</h1>
+                <Image className="c-not-found__image" src={NotFoundImg} alt="404" width={500} height={500} />
+                <h1 className="c-not-found__title">Page not found</h1>
                 <p className="c-not-found__description">Sorry, but the page you are looking for does not exist.</p>
                 <Button title="GO BACK" href="/" color="blue" />
             </section>
