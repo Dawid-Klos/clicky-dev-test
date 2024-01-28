9cecdc6c5fcc32aa2cc08c79d26c264f3503202f
Dawid Klos <klosdawid12@gmail.com>
2024-01-28 20:39:06 +0000

    feat: add simple parallax effect on Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,40 @@
+"use client";
+
+import {
+    motion,
+    useScroll,
+    useTransform,
+    useSpring
+} from "framer-motion";
+
 import Button from 'components/common/Button/Button';
 
 import './Hero.scss';
 
 
 const Hero = () => {
+    const { scrollYProgress } = useScroll();
+    const springConfig = { damping: 150, stiffness: 500 };
+
+    const h1Y = useTransform(scrollYProgress, [0, 1], [0, -2400]);
+    const pY = useTransform(scrollYProgress, [0, 1], [0, 2400]);
+    const bgY = useTransform(scrollYProgress, [0, 1], [0, -400]);
+
+    const h1SpringY = useSpring(h1Y, springConfig);
+    const pSpringY = useSpring(pY, springConfig);
+    const bgSpringY = useSpring(bgY, springConfig);
+
     return (
         <section className="c-hero">
-            <h1 className="c-hero__title">Here is a title</h1>
-            <p className="c-hero__subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget nisi a dui porttitor posuere.</p>
-            <Button title="CTA BUTTON" href="/one" color="white" />
-            <div className="c-hero__image"></div>
+            <motion.h1 style={{ x: h1SpringY }} className="c-hero__title">Here is a title</motion.h1>
+
+            <motion.p style={{ x: pSpringY }} className="c-hero__subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget nisi a dui porttitor posuere.</motion.p>
+
+            <motion.div style={{ y: -bgSpringY }}>
+                <Button title="CTA BUTTON" href="/one" color="white" />
+            </motion.div>
+
+            <motion.div style={{ y: bgSpringY }} className="c-hero__image"></motion.div>
         </section>
     );
 
